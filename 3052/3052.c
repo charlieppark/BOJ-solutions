f3ce6bae96362fc4ef9aecab991f6c21c2fbd22f
CharliePark <chanho0309@gmail.com>
2020-09-21 16:44:44 +0900

    3052 solution

--- 3052/3052.c
+++ 3052/3052.c
@@ -4,13 +4,25 @@ int main()
 {
 	int temp;
 	int arr[10] = { -1, };
+	int count = 0;
 
 	for (int i = 0; i < 10; i++)
 	{
 		scanf("%d", &temp);
-
 		arr[i] = temp % 42;
 	}
 
+	for (int k = 0; k < 42; k++)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			if (arr[i] == k)
+			{
+				count++;
+				break;
+			}
+		}
+	}
 
+	printf("%d", count);
 }
