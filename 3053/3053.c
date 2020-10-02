e695cdc575c493fdd8b372bc710eb188f0b8fc2c
CharliePark <chanho0309@gmail.com>
2020-10-02 23:00:48 +0900

    editing 3053 (unmatched)

--- 3053/3053.c
+++ 3053/3053.c
@@ -1,31 +1,15 @@
 ﻿#include <stdio.h>
 
+#define PI 3.14159265358979
+
 int main()
 {
-    int N;
+    int R;
+    while (scanf("%d", &R) != 1) continue;
 
-    int non_prime[1000] = { 0, };
+    double arith_area, taxi_area;
+    arith_area = (double)(R * R) * PI;
+    taxi_area = (double)(R * R) * 2;
 
-    while (scanf("%d", &N) != 1) continue;
-
-    for (int j = 2; j <= 499; j++)
-    {
-        for (int t = 2; t * j < 1000; t++)
-            non_prime[t * j] = 1;
-    }
-
-    non_prime[0] = non_prime[1] = 1;
-
-    int count = 0;
-
-    for (int i = 0; i < N; i++)
-    {
-        int temp;
-        while (scanf(" %d", &temp) != 1) continue;
-        if (non_prime[temp] == 0)
-        {
-            count++;
-        }
-    }
-    printf("%d", count);
+    printf("%lf\n%lf", arith_area, taxi_area);
 }
