a3e929e931ad7e150ea2ef8aec904a13113e4bce
CharliePark <chanho0309@gmail.com>
2020-10-06 21:49:10 +0900

    add 2446

---
+++ 2446/2446.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int N;
+
+    while (scanf("%d", &N) != 1) continue;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int k = 0; k < i; k++)
+            printf(" ");
+        for (int k = N; k > i; k--)
+            printf("*");
+        for (int k = N; k > i + 1; k--)
+            printf("*");
+        printf("\n");
+    }
+    for (int i = 1; i < N; i++)
+    {
+        for (int k = N - 1; k > i; k--)
+            printf(" ");
+        for (int k = 0; k < i; k++)
+            printf("*");
+        for (int k = 0; k <= i; k++)
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
