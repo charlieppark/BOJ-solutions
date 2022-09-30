b57eecf0579bc94ded0e34f9cf1db5e345154956
CharliePark <37709271+charlieppark@users.noreply.github.com>
2022-10-01 01:58:32 +0900

    [Bronze I] Title: 최소공배수, Time: 140 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int LCD(int a, int b);
+int SCM(int lcd, int a, int b);
+
+int main()
+{
+    int T;
+    
+    scanf("%d", &T);
+    
+    for (int i = 0; i < T; i++) {
+        int a, b, lcd, scm;
+        scanf("%d %d", &a, &b);
+        lcd = LCD(a, b);
+        scm = SCM(lcd, a, b);
+        printf("%d\n", scm);
+    }
+}
+
+int LCD(int a, int b)
+{
+    if (a % b == 0)
+        return b;
+
+    if (b % a == 0)
+        return a;
+
+    int temp_a = a;
+    int temp_b = b;
+    int smaller = a < b ? a : b;
+    int lcd = 1;
+
+    for (int i = 2; i < smaller; i++)
+    {
+        if (!(temp_a % i) && !(temp_b % i))
+        {
+            temp_a /= i;
+            temp_b /= i;
+            smaller /= i;
+            lcd *= i;
+            i--;
+        }
+    }
+
+    return lcd;
+}
+
+int SCM(int lcd, int a, int b)
+{
+    if (a % b == 0)
+        return a;
+
+    if (b % a == 0)
+        return b;
+
+    int scm = 1;
+
+    scm *= lcd;
+    scm *= (a / lcd);
+    scm *= (b / lcd);
+
+    return scm;
+}
