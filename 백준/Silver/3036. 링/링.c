a602a26c32f102d8d090e964c760d2a950bf039a
CharliePark <37709271+charlieppark@users.noreply.github.com>
2022-11-12 11:43:06 +0900

    [Silver IV] Title: 링, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/3036. 링/링.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int N;
+    
+    scanf("%d", &N);
+    
+    int* arr = malloc(sizeof(*arr) * N);
+    
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &(arr[i]));
+    }
+    
+    for (int i = 1; i < N; i++) {
+        int a, b;
+        a = arr[0];
+        b = arr[i];
+        
+        int divider = 1;
+        
+        int larger = a > b ? a : b;
+        
+        for (int j = 2; j <= a && j <= b; j++)
+        {
+            if ((a % j == 0) && (b % j == 0))
+            {
+                divider *= j;
+                a /= j;
+                b /= j;
+                j = 1;
+            }
+    
+        }
+        a = arr[0];
+        b = arr[i];
+        
+        printf("%d/%d\n", a / divider, b / divider);
+    }
+}
